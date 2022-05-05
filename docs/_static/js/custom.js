9ce70d3d53188a4a486e08b60e2545e89c44161d
zhyantao <zh6tao@gmail.com>
2022-05-05 22:26:54 +0800

    Test sphinx comments module

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -4,3 +4,22 @@ $(document).ready(
         $('a.external').attr('target', '_blank');
     }
 );
+
+// Add sphinx comment module
+var script = document.createElement("script");
+script.type = "text/javascript";
+script.src = "https://utteranc.es/client.js";
+script.async = "async";
+
+script.setAttribute("repo", "zhyantao/zhyantao.github.io");
+script.setAttribute("issue-term", "pathname");
+script.setAttribute("theme", "github-light");
+script.setAttribute("label", "comments");
+script.setAttribute("crossorigin", "anonymous");
+
+// sections = document.querySelectorAll("div.section");
+sections = document.querySelectorAll("div>section");
+if (sections !== null) {
+    section = sections[sections.length-1];
+    section.appendChild(script);
+}
