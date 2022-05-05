c0d9d47202047ce000dcd75f58e654f24d2f4e7f
zhyantao <zh6tao@gmail.com>
2022-05-06 00:10:38 +0800

    Fix sphinx comment (utterances) module

---
+++ scripts/fix-comments.py
@@ -0,0 +1,25 @@
+import os
+
+html_list = []
+def collect_html(path):
+    for item in os.listdir(path):
+        new_path = path + '/' + item
+        if os.path.isdir(new_path):
+            collect_html(new_path)
+        else:
+            _, ext = os.path.splitext(new_path)
+            if ext == '.html':
+                html_list.append(new_path)
+
+collect_html('build/html')
+for html_file in html_list:
+    html_content = ""
+    with open(html_file, 'r', encoding='UTF-8') as f:
+        html_content_lines = f.readlines()
+        for line in html_content_lines:
+            html_content += line
+    print(html_content)
+    if html_content.find('div.section') != -1:
+        html_content = html_content.replace('div.section', 'div>section')
+        with open(html_file, 'w', encoding='UTF-8') as f:
+            f.write(html_content)
